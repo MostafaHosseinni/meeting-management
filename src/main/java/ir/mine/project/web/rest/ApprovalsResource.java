138e0e009dfc65486057798811b6ebcbbe1ebbcc
MostafaHosseinni <mhcr.72428@yahoo.com>
2023-01-23 17:44:26 +0330

    init project

--- src/main/java/ir/mine/project/web/rest/ApprovalsResource.java
+++ src/main/java/ir/mine/project/web/rest/ApprovalsResource.java
@@ -28,7 +28,6 @@ import ir.mine.project.web.rest.util.PaginationUtil;
 @RequestMapping("/Approvals")
 public class ApprovalsResource extends BaseRestFulServiceSecure<Approvals, ApprovalsDTO, Long, ApprovalsService , TestBriefDTO> {
 	private static final String ENTITY_NAME = "approvals";
-
 	public ApprovalsResource(ApprovalsService approvalsService) {
 		super(approvalsService);
 		setENTITY_NAME(ENTITY_NAME);
